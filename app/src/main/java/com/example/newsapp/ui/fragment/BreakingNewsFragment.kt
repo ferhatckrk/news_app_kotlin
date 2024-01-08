4c9d20605571d11b1880d217c7862bae87dcca7d
Ferhat Çıkrık <ferhatckrk@gmail.com>
2024-01-08 03:31:07 +0300

    nothing

--- app/src/main/java/com/example/newsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragment/BreakingNewsFragment.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -15,27 +17,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
 import com.example.newsapp.adapters.NewsAdapter
+import com.example.newsapp.databinding.FragmentBreakingNewsBinding
+import com.example.newsapp.model.NewsResponse
 import com.example.newsapp.ui.NewsActivity
 import com.example.newsapp.ui.NewsViewModel
 import com.example.newsapp.util.Resource
-import com.example.newsapp.databinding.FragmentBreakingNewsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.io.Console
 
 
 @AndroidEntryPoint
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
-
-
-    lateinit var viewModel: NewsViewModel
-    lateinit var newsAdapter: NewsAdapter
-
-    lateinit var recyclerView: RecyclerView
-    lateinit var progressBar: ProgressBar
-
-    val TAG="BreakingNewsFragment"
-
 
     private val viewModel by viewModels<NewsViewModel>()
     lateinit var newsAdapter: NewsAdapter
@@ -57,62 +51,9 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
     }
 
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        recyclerView = view.findViewById<RecyclerView>(R.id.rvBreakingNews)
-        progressBar = view.findViewById<ProgressBar>(R.id.paginationProgressBar)
-
-
-        viewModel = (activity as NewsActivity).viewModel
         setupRecyclerView()
-
-
-        viewModel .breakingnews.observe(viewLifecycleOwner, Observer { response ->
-            when (response) {
-                is Resource.Success -> {
-                    hideProgresBar()
-                    response.data?.let {
-                        newsResponse->
-                        newsAdapter.differ.submitList(newsResponse.articles)
-                    }
-                }
-
-                is Resource.Error -> {
-                   hideProgresBar()
-                    response.message?.let {
-                        message->
-                         Log.e(TAG, "An error occurrend: $message" )
-                    }
-                }
-                is Resource.Loading-> {
-                    showProgressBar()
-                }
-            }
-        })
-    }
-
-
-
-    private fun hideProgresBar() {
-        progressBar.visibility = View.INVISIBLE
-    }
-
-    private fun showProgressBar() {
-        progressBar.visibility = View.VISIBLE
-    }
-
-
-    private fun setupRecyclerView() {
-        newsAdapter = NewsAdapter()
-        recyclerView.apply {
-            adapter = newsAdapter
-            layoutManager = LinearLayoutManager(activity)
-
-        }
-
-         setupRecyclerView()
 
         viewModel.getBreakingNews("us")
 
@@ -210,7 +151,6 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
     private fun showProgressBar() {
         binding.paginationProgressBar.visibility = View.VISIBLE
 
-
     }
 
 
@@ -226,5 +166,4 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
 
 }
-
 
